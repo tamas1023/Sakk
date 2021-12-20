6fd84ce2ce3a9cb840b6f0b29b5ac335a82a5815
thespekky <bandras123456@gmail.com>
2021-12-20 10:33:18 +0100

    futó lépésének elkezdése és a meglévő lépéseknek az ütésének megírása

--- Sakk/Babuk.cs
+++ Sakk/Babuk.cs
@@ -24,6 +24,7 @@ namespace Sakk
         }
         public void lephete(Babuk[,] babok,int i,int j,List<int>lepesek)
         {
+            bool ciklusseged = true;
             if (tipus == "huszár")
             {
                 if (i - 2 >= 0 && j - 1 >= 0)
@@ -93,30 +94,270 @@ namespace Sakk
             }
             if(tipus=="gyalog"&&szin=="fehér")
             {
-                if(j==1)
+                if (j == 1 && babok[i, j + 2].id == -1)
                 {
-                    lepesek.Add(i);
-                    lepesek.Add(j + 1);
                     lepesek.Add(i);
                     lepesek.Add(j + 2);
                 }
-                if(j!=1)
+                if(j == 1 && babok[i, j + 1].id == -1)
+                { 
+                    lepesek.Add(i);
+                    lepesek.Add(j + 1);
+                }
+                if (j!=1 && babok[i, j + 1].id == -1)
                 {
                     lepesek.Add(i);
                     lepesek.Add(j + 1);
                 }
             }
+            if (tipus == "gyalog" && szin == "fekete")
+            {
+                if (j == 6 &&babok[i,j-2].id==-1)
+                {
+                    lepesek.Add(i);
+                    lepesek.Add(j - 2);
+                }
+
+                if(j==6&&babok[i,j-1].id==-1)
+                {
+                    lepesek.Add(i);
+                    lepesek.Add(j - 1);
+                }
+                if (j != 6 && babok[i, j-1].id == -1)
+                {
+                    lepesek.Add(i);
+                    lepesek.Add(j - 1);
+                }
+            }
+            if(tipus== "futó")
+            {
+                for (int segedj = j; segedj < 8; segedj++)
+                {
+                    if(!ciklusseged)
+                    {
+                        break;
+                    }
+                    for (int segedi = i; segedi < 8; segedi++)
+                    {
+                        if(babok[segedi,segedi].id!=-1)
+                        {
+                            lepesek.Add(segedi);
+                            lepesek.Add(segedj);
+                        }
+                        if(babok[segedi, segedi].id == -1)
+                        {
+                            ciklusseged = false;
+                            break;
+                            
+                        }
+                    }
+                }
+                ciklusseged = true;
+                for (int segedj = j; segedj >= 0; segedj--)
+                {
+                    if (!ciklusseged)
+                    {
+                        break;
+                    }
+                    for (int segedi = i; segedi < 8; segedi++)
+                    {
+                        if (babok[segedi, segedi].id != -1)
+                        {
+                            lepesek.Add(segedi);
+                            lepesek.Add(segedj);
+                        }
+                        if (babok[segedi, segedi].id == -1)
+                        {ciklusseged = false;
+                            break;
+                            
+                        }
+                    }
+                }
+                ciklusseged = true;
+                for (int segedj = j; segedj >= 0; segedj--)
+                {
+                    if (!ciklusseged)
+                    {
+                        break;
+                    }
+                    for (int segedi = i; segedi >= 0; segedi--)
+                    {
+                        if (babok[segedi, segedi].id != -1)
+                        {
+                            lepesek.Add(segedi);
+                            lepesek.Add(segedj);
+                        }
+                        if (babok[segedi, segedi].id == -1)
+                        {
+                            ciklusseged = false;
+                            break;
+
+                        }
+                    }
+                }
+                ciklusseged = true;
+                for (int segedj = j; segedj < 8; segedj++)
+                {
+                    if (!ciklusseged)
+                    {
+                        break;
+                    }
+                    for (int segedi = i; segedi >= 0; segedi--)
+                    {
+                        if (babok[segedi, segedi].id != -1)
+                        {
+                            lepesek.Add(segedi);
+                            lepesek.Add(segedj);
+                        }
+                        if (babok[segedi, segedi].id == -1)
+                        {
+                            ciklusseged = false;
+                            break; 
+                        }
+                    }
+                }
+                ciklusseged = true;
+            }
 
         }
-        public bool huszar()
+        public void uthete(Babuk[,] babok, int i, int j, List<int> utesek)
         {
             if (tipus == "huszár")
             {
-                return true;
+                if (i - 2 >= 0 && j - 1 >= 0)
+                {
+                    if (babok[i - 2, j - 1].id != -1&&szin!=babok[i-2,j-1].szin)
+                    {
+                        utesek.Add(i - 2);
+                        utesek.Add(j - 1);
+                    }
+                }
+                if (i - 2 >= 0 && j + 1 <= 7)
+                {
+                    if (babok[i - 2, j + 1].id != -1 && szin != babok[i-2, j+1].szin)
+                    {
+                        utesek.Add(i - 2);
+                        utesek.Add(j + 1);
+                    }
+                }
+                if (i + 2 <= 7 && j + 1 <= 7)
+                {
+                    if (babok[i + 2, j + 1].id != -1 && szin != babok[i+2, j+1].szin)
+                    {
+                        utesek.Add(i + 2);
+                        utesek.Add(j + 1);
+                    }
+                }
+                if (i + 2 <= 7 && j - 1 >= 0)
+                {
+                    if (babok[i + 2, j - 1].id != -1 && szin != babok[i+2, j-1].szin)
+                    {
+                        utesek.Add(i + 2);
+                        utesek.Add(j - 1);
+                    }
+                }
+                if (i + 1 <= 7 && j - 2 >= 0)
+                {
+                    if (babok[i + 1, j - 2].id != -1 && szin != babok[i+1, j-2].szin)
+                    {
+                        utesek.Add(i + 1);
+                        utesek.Add(j - 2);
+                    }
+                }
+                if (i - 1 >= 0 && j - 2 >= 0)
+                {
+                    if (babok[i - 1, j - 2].id != -1 && szin != babok[i-1, j-2].szin)
+                    {
+                        utesek.Add(i - 1);
+                        utesek.Add(j - 2);
+                    }
+                }
+                if (i - 1 >= 0 && j + 2 <= 7)
+                {
+                    if (babok[i - 1, j + 2].id != -1 && szin != babok[i-1, j+2].szin)
+                    {
+                        utesek.Add(i - 1);
+                        utesek.Add(j + 2);
+                    }
+                }
+                if (i + 1 <= 7 && j + 2 <= 7)
+                {
+                    if (babok[i + 1, j + 2].id != -1 && szin != babok[i+1, j+2].szin)
+                    {
+                        utesek.Add(i + 1);
+                        utesek.Add(j + 2);
+                    }
+                }
             }
-            else
+            if (tipus == "gyalog" && szin == "fehér")
             {
-                return false;
+               if(i-1>=0)
+                { 
+                    if (j == 1 && babok[i-1, j + 1].id != -1 && szin != babok[i -1, j + 1].szin)
+                    {
+                        utesek.Add(i-1);
+                        utesek.Add(j + 1);
+                    }
+                }
+               if(i+1<=7)
+                { 
+                    if (j == 1 && babok[i+1, j + 1].id != -1 && szin != babok[i + 1, j + 1].szin)
+                    {
+                        utesek.Add(i + 1);
+                        utesek.Add(j + 1);
+                    }
+                }
+                if (i-1>=0)
+                { 
+                    if (j != 1 && babok[i - 1, j + 1].id != -1 && szin != babok[i - 1, j + 1].szin)
+                    {
+                        utesek.Add(i - 1);
+                        utesek.Add(j + 1);
+                    }
+                }
+                if(i+1<=7)
+                { 
+                    if (j != 1 && babok[i + 1, j + 1].id != -1 && szin != babok[i + 1, j + 1].szin)
+                    {
+                        utesek.Add(i + 1);
+                        utesek.Add(j + 1);
+                    }
+                }
+            }
+            if (tipus == "gyalog" && szin == "fekete")
+            {
+                if(i-1>=0)
+                { 
+                    if (j == 6 && babok[i - 1, j - 1].id != -1 && szin != babok[i - 1, j - 1].szin)
+                    {
+                        utesek.Add(i - 1);
+                        utesek.Add(j - 1);
+                    }
+                }
+                if(i+1<=7)
+                { 
+                    if (j == 6 && babok[i + 1, j - 1].id != -1 && szin != babok[i + 1, j - 1].szin)
+                    {
+                        utesek.Add(i + 1);
+                        utesek.Add(j - 1);
+                    }
+                }
+                if (i-1>=0)
+                { 
+                    if (j != 6 && babok[i - 1, j - 1].id != -1 && szin != babok[i - 1, j - 1].szin)
+                    {
+                        utesek.Add(i - 1);
+                        utesek.Add(j - 1);
+                    }
+                }
+                if(i+1<=7)
+                { 
+                    if (j != 6 && babok[i + 1, j - 1].id != -1 && szin != babok[i + 1, j - 1].szin)
+                    {
+                        utesek.Add(i + 1);
+                        utesek.Add(j - 1);
+                    }
+                }
             }
         }
 
--- Sakk/Form1.cs
+++ Sakk/Form1.cs
@@ -17,8 +17,11 @@ namespace Sakk
         static int honnani;
         static int honnanj;
         static bool valaszt = true;
+        static bool feher = true;
         static bool uthet = true;
+        static bool sakk = false;
         static List<int> lephetlista = new List<int>();
+        static List<int> uthetlista = new List<int>();
         public Form1()
         {
             InitializeComponent();
@@ -50,16 +53,38 @@ namespace Sakk
             kepek[6, 0].Image = Image.FromFile("../../img/white_knight.png");
             babok[7, 0] = new Babuk(7, "bástya", "fehér", 7, 0);
             kepek[7, 0].Image = Image.FromFile("../../img/white_rook.png");
+
+
+            babok[0, 7] = new Babuk(24, "bástya", "fekete", 0, 7);
+            kepek[0, 7].Image = Image.FromFile("../../img/black_rook.png");
+            babok[1, 7] = new Babuk(25, "huszár", "fekete", 1, 7);
+            kepek[1, 7].Image = Image.FromFile("../../img/black_knight.png");
+            babok[2, 7] = new Babuk(26, "futó", "fekete", 2, 7);
+            kepek[2, 7].Image = Image.FromFile("../../img/black_bishop.png");
+            babok[3, 7] = new Babuk(27, "vezér", "fekete", 3, 7);
+            kepek[3, 7].Image = Image.FromFile("../../img/black_queen.png");
+            babok[4, 7] = new Babuk(28, "király", "fekete", 4, 7);
+            kepek[4, 7].Image = Image.FromFile("../../img/black_king.png");
+            babok[5, 7] = new Babuk(29, "futó", "fekete", 5, 7);
+            kepek[5, 7].Image = Image.FromFile("../../img/black_bishop.png");
+            babok[6, 7] = new Babuk(30, "huszár", "fekete", 6, 7);
+            kepek[6, 7].Image = Image.FromFile("../../img/black_knight.png");
+            babok[7, 7] = new Babuk(31, "bástya", "fekete", 7, 7);
+            kepek[7, 7].Image = Image.FromFile("../../img/black_rook.png");
             for (int j = 0; j <8; j++)
             {
                 for (int i = 0; i < 8; i++)
                 {
-
                     if (j==1)
                     {
-                        babok[i,j] = new Babuk(i, "gyalog", "fehér",i,j);
+                        babok[i,j] = new Babuk(i+8, "gyalog", "fehér",i,j);
                         kepek[i, j].Image = Image.FromFile("../../img/white_pawn.png");
                      }
+                    if (j == 6)
+                    {
+                        babok[i, j] = new Babuk(i + 16, "gyalog", "fekete", i, j);
+                        kepek[i, j].Image = Image.FromFile("../../img/black_pawn.png");
+                    }
                 }
             }
             
@@ -149,41 +174,71 @@ namespace Sakk
 
             if(valaszt)
             {
-                if(babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Id >= 0)
-                {
-                torolszinek();
-                honnani = Convert.ToInt32(kapcsolt.Tag);
-                honnanj = Convert.ToInt32(kapcsolt.Name);
-                babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].lephete(babok, Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name), lephetlista);
-                szinezes();
-                }
-                if (valaszt && kapcsolt.Image == null)
-                {
-                    valaszt = false;
-                }
-            }
-           
-            if(!valaszt)
-            {
-                // MessageBox.Show(babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Szin + "típus: " + babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Tipus);
-                //MessageBox.Show("name: babok[honnani,honnanj].huszar()j:" +kepek[Convert.ToInt32(kapcsolt.Tag),Convert.ToInt32(kapcsolt.Name)].Name+"tag: i: "+ kepek[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Tag);
-
-               
-                if (babok[honnani, honnanj].Tipus=="huszár")
-                {
-                    MessageBox.Show("huszár");
+                if(feher)
+                { 
+                    if(babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Id >= 0&& babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Szin=="fehér")
+                    {
+                    torolszinek();
+                    honnani = Convert.ToInt32(kapcsolt.Tag);
+                    honnanj = Convert.ToInt32(kapcsolt.Name);
+                        MessageBox.Show("id:" +babok[honnani,honnanj].Id);
+                    babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].lephete(babok, Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name), lephetlista);
+                        babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].uthete(babok, Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name), uthetlista);
+                        szinezes();
+                    }
+                    if (valaszt && kapcsolt.Image == null)
+                    {
+                        valaszt = false;
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("nem huszár");
+                    if (babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Id >= 0 && babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Szin == "fekete")
+                    {
+                        torolszinek();
+                        honnani = Convert.ToInt32(kapcsolt.Tag);
+                        honnanj = Convert.ToInt32(kapcsolt.Name);
+                        MessageBox.Show("id:" + babok[honnani, honnanj].Id);
+                        babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].uthete(babok, Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name), uthetlista);
+                        babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].lephete(babok, Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name), lephetlista);
+                        szinezes();
+                    }
+                    if (valaszt && kapcsolt.Image == null)
+                    {
+                        valaszt = false;
+                    }
                 }
+            }
+           
+            if(!valaszt&&kapcsolt.BackColor==Color.Orange)
+            {
+                lepes(Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name));
+                //MessageBox.Show(babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Szin + "típus: " + babok[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Tipus);
+               // MessageBox.Show("name: babok[honnani,honnanj].huszar()j:" +kepek[Convert.ToInt32(kapcsolt.Tag),Convert.ToInt32(kapcsolt.Name)].Name+"tag: i: "+ kepek[Convert.ToInt32(kapcsolt.Tag), Convert.ToInt32(kapcsolt.Name)].Tag);
 
-                
+                if(feher)
+                { 
+                    feher = false; 
+                }
+                else
+                {
+                    feher = true;
+                }
                // MessageBox.Show("teszt:" + lephetlista[0]);
                 valaszt = true;
                 torolszinek();
             }
          
+        }
+
+        private void lepes(int i,int j)
+        {
+           
+            kepek[i, j].Image = kepek[honnani, honnanj].Image;
+            kepek[honnani, honnanj].Image = null;
+
+            babok[i, j] = babok[honnani, honnanj];
+            babok[honnani, honnanj]= babok[honnani, honnanj] = new Babuk(-1, "", "", 0, 0);
         }
 
         private void szinezes()
@@ -213,16 +268,40 @@ namespace Sakk
                     db = 0;
                 }
             }
+            segedi = 0;
+            segedj = 0;
+            for (int i = 0; i < uthetlista.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    //MessageBox.Show("belépett osztva 0");
+                    segedi = uthetlista[i];
+                    db++;
+                }
+                if (i % 2 == 1)
+                {
+                    // MessageBox.Show("belépett osztva 1");
+                    segedj = uthetlista[i];
+                    db++;
+                }
+                if (db == 2)
+                {
+                    //MessageBox.Show("szinezés:" + segedi + " " + segedj);
+                    kepek[segedi, segedj].BackColor = Color.Red;
+                    db = 0;
+                }
+            }
         }
 
         private void torolszinek()
         {
             lephetlista.Clear();
+            uthetlista.Clear();
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0;  j < 8;  j++)
                 {
-                    if(kepek[i,j].BackColor==Color.Orange)
+                    if(kepek[i,j].BackColor==Color.Orange||kepek[i,j].BackColor==Color.Red)
                     {
                         if (i % 2 == 0)
                         {
